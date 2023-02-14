2f2bec3a5ab63dfedd1f0c30b08c3ad12bf849aa
AmanPathak-dev <aman07pathak@gmail.com>
2023-02-14 14:18:52 +0530

    Webapps Configured through Pull Github Repo

---
+++ Webapps-Configured-Github/main.tf
@@ -0,0 +1,40 @@
+locals {
+  resource_group_name = "RG-for-WebApss"
+  location            = "North Europe"
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = local.resource_group_name
+  location = local.location
+}
+
+resource "azurerm_app_service_plan" "service_plan" {
+  name                = "new-service-plan"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+
+  depends_on = [
+    azurerm_resource_group.resource_group
+  ]
+}
+
+resource "azurerm_app_service" "app_service" {
+  name                = "the-unique-one-service-app"
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  app_service_plan_id = azurerm_app_service_plan.service_plan.id
+  site_config {
+    dotnet_framework_version = "v6.0"
+  }
+
+  source_control {
+    repo_url           = "https://github.com/alashro/webapp"
+    branch             = "master"
+    manual_integration = true
+    use_mercurial      = false
+  }
+}
